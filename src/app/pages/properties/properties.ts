89b01203250a7b35fde93ba4611318ffb699b64a
Anuradha-117 <m.anuradha.lakruwan@gmail.com>
2026-05-26 23:10:27 +0530

    implemented feature to fetch and display property listings

--- src/app/pages/properties/properties.ts
+++ src/app/pages/properties/properties.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Property } from '../../services/property'; 
 
 @Component({
   selector: 'app-properties',
+  standalone: true,
   imports: [],
   templateUrl: './properties.html',
-  styleUrl: './properties.css',
+  styleUrl: './properties.css'
 })
-export class Properties {}
+export class Properties implements OnInit {
+  propertyList = signal<any[]>([]);
+
+  constructor(private propertyService: Property) {}
+
+  ngOnInit(): void {
+    this.propertyService.getAllProperties().subscribe({
+      next: (data) => {
+        this.propertyList.set(data); 
+      },
+      error: (err) => {
+        console.error('Error fetching properties', err);
+      }
+    });
+  }
+}
