85518a6c414b7c04895aee179a7e05b636a7658f
Anuradha-117 <m.anuradha.lakruwan@gmail.com>
2026-05-26 17:01:53 +0530

    configred angular routing for core pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home} from './pages/home/home'; 
+import { Properties} from './pages/properties/properties';
+import { Login } from './pages/login/login';
+import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'properties', component: Properties },
+  { path: 'login', component: Login},
+  { path: 'admin', component: AdminDashboard }
+];
