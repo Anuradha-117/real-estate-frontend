d12d64e6457e2c5a89a10a61594fc322a993663d
Anuradha-117 <m.anuradha.lakruwan@gmail.com>
2026-05-27 15:21:12 +0530

    implemented dynamic status filtering for property listings

--- src/app/pages/properties/properties.ts
+++ src/app/pages/properties/properties.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { Property } from '../../services/property'; 
+import { Component, OnInit, signal, computed } from '@angular/core';
+import { Property } from '../../services/property';
 
 @Component({
   selector: 'app-properties',
@@ -10,17 +10,30 @@ import { Property } from '../../services/property';
 })
 export class Properties implements OnInit {
   propertyList = signal<any[]>([]);
+  currentFilter = signal<string>('Available');
+  filteredProperties = computed(() => {
+    const list = this.propertyList();
+    const filter = this.currentFilter();
 
-  constructor(private propertyService: Property) {}
+    if (filter === 'All') return list;
+    return list.filter(p => p.status === filter);
+  });
+
+  constructor(private propertyService: Property) { }
 
   ngOnInit(): void {
     this.propertyService.getAllProperties().subscribe({
       next: (data) => {
-        this.propertyList.set(data); 
+        this.propertyList.set(data);
       },
       error: (err) => {
         console.error('Error fetching properties', err);
       }
     });
   }
+
+  setFilter(status: string) {
+    this.currentFilter.set(status);
+  }
+
 }
