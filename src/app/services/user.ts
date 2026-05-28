ebfa511f7929abb5f4ccb748ed2d8c8c6a9fdc33
Anuradha-117 <m.anuradha.lakruwan@gmail.com>
2026-05-28 09:58:27 +0530

    implemented user service for login and registration

---
+++ src/app/services/user.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+    private apiUrl = 'http://localhost:8080/api/users';
+    constructor(private http: HttpClient) { }
+
+    // Sending usr data to bcknd
+    register(userData: any): Observable<any> {
+        return this.http.post(`${this.apiUrl}/register`, userData);
+    }
+
+    // send email and password to match
+    login(credentials: any): Observable<any> {
+        return this.http.post(`${this.apiUrl}/login`, credentials);
+    }
+}
