88e7f27bb6dc6c7404afb279d7911cc5ba6052be
Anuradha-117 <m.anuradha.lakruwan@gmail.com>
2026-05-28 11:46:27 +0530

    implemented dynamic navbar with rolebased visbility and logout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('real-estate-frontend');
+export class AppComponent {
+  title = 'Real Estate System';
+
+  constructor(private router: Router) { }
+
+  get isLoggedIn(): boolean {
+    return !!localStorage.getItem('loggedUser');
+  }
+
+  get isAdmin(): boolean {
+    const userStr = localStorage.getItem('loggedUser');
+    if (userStr) {
+      const user = JSON.parse(userStr);
+      return user.role && user.role.toLowerCase() === 'admin';
+    }
+    return false;
+  }
+
+  logout() {
+    localStorage.removeItem('loggedUser');
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/services/auth-guard.ts
+++ src/app/services/auth-guard.ts
@@ -5,17 +5,14 @@ export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const userStr = localStorage.getItem('loggedUser');
 
-  // 1. Check if anyone is logged in
   if (userStr) {
     const user = JSON.parse(userStr);
-    
-    // 2. Are they an Admin?
+
     if (user.role && user.role.toLowerCase() === 'admin') {
-      return true; // The door opens!
+      return true;
     }
   }
 
-  // 3. KICK THEM OUT!
   alert('Access Denied! You must be an Administrator to view this page.');
   router.navigate(['/login']);
   return false;
