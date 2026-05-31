e9df5d5b496373898e24f0b8df755d4b11a7c080
Anuradha-117 <m.anuradha.lakruwan@gmail.com>
2026-05-31 09:30:12 +0530

    convert admin dashboard layout to tabs

--- src/app/pages/admin-dashboard/admin-dashboard.ts
+++ src/app/pages/admin-dashboard/admin-dashboard.ts
@@ -14,6 +14,7 @@ import { Router } from '@angular/router';
 })
 export class AdminDashboard implements OnInit { 
 
+  activeTab: string = 'addProperty';
   propertyObj: any = {
     title: '',
     location: '',
